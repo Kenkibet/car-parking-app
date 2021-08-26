631b5d1f3227a0dd70a9585b7a758c16898e822c
saretoduncan <saretoduncan@gmail.com>
2021-08-26 13:25:26 +0300

    complete backend

---
+++ src/main/java/modules/Vehicle.java
@@ -0,0 +1,16 @@
+package modules;
+
+import java.sql.Timestamp;
+
+public class Vehicle {
+    private String type;
+    private String numberPlate;
+    private String  model;
+    private Timestamp checkin;
+    private Timestamp checkout;
+    private int id;
+    public Vehicle(String type, String numberPlate, String model){
+
+    }
+
+}
---
+++ src/test/java/modules/VehicleTest.java
@@ -0,0 +1,47 @@
+package modules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class VehicleTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void getNumberPlate() {
+    }
+
+    @Test
+    void setNumberPlate() {
+    }
+
+    @Test
+    void getModel() {
+    }
+
+    @Test
+    void setModel() {
+    }
+
+    @Test
+    void getCheck_in() {
+    }
+
+    @Test
+    void getCheck_out() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+}
