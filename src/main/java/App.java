86581285ec8644d70f87ccae0927dffc87525b0d
saretoduncan <saretoduncan@gmail.com>
2021-08-27 06:14:01 +0300

    add api access control

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,40 @@
 import com.google.gson.Gson;
+import exceptions.ApiException;
 import modules.Vehicle;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
  public static void main(String[] args){
+     port(getHerokuAssignedPort());
+
      Gson gson = new Gson();
+     options("/*", (request, response) -> {
+
+         String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+         if (accessControlRequestHeaders != null) {
+             response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+         }
+
+         String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+         if (accessControlRequestMethod != null) {
+             response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+         }
+         return "OK";
+     });
+
+     before((request, response) -> {
+         response.header("Access-Control-Allow-Origin", "*");
+     });
      //Add vehicles in the packing lot
      post("/api/packVehicle/new", "application/json", (request, response) -> {
          Vehicle vehicle = gson.fromJson(request.body(),Vehicle.class);
@@ -22,6 +52,11 @@ public class App {
          System.out.println(vehicleType);
          return gson.toJson(Vehicle.findByType(vehicleType));
      });
+     get("/api/packVehicle/find/:vehicleId","application/json",(request, response) -> {
+         int vehicleId= Integer.parseInt(request.params("vehicleId"));
+         return gson.toJson(Vehicle.findById(vehicleId));
+
+     });
      //checkout
      post("/api/packVehicle/checkout/:vehicleId", "application/json", (request, response) -> {
          int vehicleId= Integer.parseInt(request.params("vehicleId"));
@@ -30,6 +65,7 @@ public class App {
          return gson.toJson(Vehicle.getAllVehicles());
 
      });
+
      //remove vehicle from database
      post("/api/packVehicle/delete/:vehicleId", "application/json", (request, response) -> {
          int vehicleId= Integer.parseInt(request.params("vehicleId"));
@@ -46,6 +82,15 @@ public class App {
      //FILTERS
      after((req, res) ->{
          res.type("application/json");
+     });
+     exception(ApiException.class, (exc, req, res) -> {
+         ApiException err = (ApiException) exc;
+         Map<String, Object> jsonMap = new HashMap<>();
+         jsonMap.put("status", err.getStatusCode());
+         jsonMap.put("errorMessage", err.getMessage());
+         res.type("application/json"); //after does not run in case of an exception.
+         res.status(err.getStatusCode()); //set the status
+         res.body(gson.toJson(jsonMap));  //set the output.
      });
  }
 
--- src/main/java/modules/DB.java
+++ src/main/java/modules/DB.java
@@ -3,5 +3,6 @@ package modules;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public  static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/packing_db","softwaredev","1234");
+//    public  static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/packing_db","softwaredev","1234");
+    public  static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-156-151-232.compute-1.amazonaws.com:5432/d6j9qtlqq03pja","nhhsxzpseoxevq","59465bfebd20be674f593f40f47ce83435c33e3394525cb0da8f5c865a6d9c20");
 }
